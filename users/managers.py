c09de5f421a50e16f107b0427d6294e81aaa5d35
A-Burnhard <benadkojo@gmail.com>
2023-09-11 16:27:52 +0000

    after second migration

--- users/managers.py
+++ users/managers.py
@@ -18,18 +18,19 @@ class AccountManager(BaseUserManager):
         user.save()
         return user
     
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email, and password.
-        """
-        extra_fields.setdefault('is_admin', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-        
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        
-        return self.create_user(email, password, **extra_fields)
+def create_superuser(self, email, password, **extra_fields):
+    """
+    Create and save a SuperUser with the given email, and password.
+    """
+    extra_fields.setdefault('user_type', 1)  # setting user_type to 'admin'
+    extra_fields.setdefault('is_admin', True)
+    extra_fields.setdefault('is_staff', True)
+    extra_fields.setdefault('is_superuser', True)
+    extra_fields.setdefault('is_active', True)
+    
+    if extra_fields.get('is_staff') is not True:
+        raise ValueError('Superuser must have is_staff=True.')
+    if extra_fields.get('is_superuser') is not True:
+        raise ValueError('Superuser must have is_superuser=True.')
+    
+    return self.create_user(email, password, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-# trunk-ignore(flake8/F401)
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from .managers import AccountManager
 
 class User(AbstractUser):
@@ -9,33 +8,26 @@ class User(AbstractUser):
         (2, 'instructor'),
         (3, 'student'),
     )
-    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=1)
-    username = None
+    
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=3)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
     email = models.EmailField(('Email address'), unique=True)
-    first_name = models.CharField(max_length=25)
     surname = models.CharField(max_length=25)
     gender = models.CharField(max_length=10)
     phone = models.CharField(max_length=20)
     dob = models.DateField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
-    is_admin = models.BooleanField(default=False)
-    is_customer = models.BooleanField(default = False)
-    is_staff = models.BooleanField(default=False)
-    is_courier = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-    objects = AccountManager()
     
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # here you can list required fields for the createsuperuser command
+    objects = AccountManager()
+
     class Meta:
         db_table = 'users'
 
-    def get_name(self):
-        return self.fullname or self.email
+    def get_full_name(self):
+        return f"{self.first_name} {self.surname}"
 
     def __str__(self):
         return self.email
@@ -46,6 +38,6 @@ class Phone(models.Model):
     number = models.CharField(max_length=20)
     otp = models.CharField(max_length=6)
     verified = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.number
